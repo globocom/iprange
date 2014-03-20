5e61dc776adaa88891eed22b7bc05308b98e0d39
Juarez Bochi <jbochi@gmail.com>
2014-03-20 16:49:45 -0300

    Do not remove before any insertion

--- lib/iprange.rb
+++ lib/iprange.rb
@@ -9,15 +9,34 @@ module IPRange
       @redis_key = redis_key
     end
 
-    def add(range)
-      end_ip = IPAddr.new(range).to_range.last.to_i
-      @redis.zadd(@redis_key, end_ip, range)
+    def remove(range)
+      @redis.zrem(@redis_key, range_key(range))
+      @redis.del(metadata_key(range))
+    end
+
+    def add(range, metadata={})
+      key = range_key(range)
+      @redis.zadd(@redis_key, key, range)
+      hash = metadata_key(range)
+      @redis.mapped_hmset(hash, metadata) unless metadata.empty?
     end
 
     def find(ip)
       ipaddr = IPAddr.new(ip)
       next_range = @redis.zrangebyscore(@redis_key, ipaddr.to_i, "+inf", limit: [0, 1]).first
-      next_range if IPAddr.new(next_range).include? ipaddr
+      if IPAddr.new(next_range).include? ipaddr
+        metadata = @redis.hgetall(metadata_key(next_range))
+        {range: next_range}.merge(metadata)
+      end
+    end
+
+    private
+    def range_key(range)
+      IPAddr.new(range).to_range.last.to_i
+    end
+
+    def metadata_key(range)
+      "#{@redis_key}:#{range}"
     end
   end
 end
--- spec/iprange_spec.rb
+++ spec/iprange_spec.rb
@@ -24,19 +24,41 @@ describe IPRange::Range do
     end
 
     before do
+      subject.remove("192.168.0.1/24")
       subject.add("192.168.0.1/24")
     end
 
     it "should find it back" do
-      expect(subject.find "192.168.0.20").to eq("192.168.0.1/24")
+      response = subject.find("192.168.0.20")
+      expect(response).to eq({range: "192.168.0.1/24"})
     end
 
     it "should return nil for smaller ip that is not in range" do
-      expect(subject.find "192.167.255.255").to be_nil
+      response = subject.find("192.167.255.255")
+      expect(response).to be_nil
     end
 
     it "should return nil for greater ip that is not in range" do
-      expect(subject.find "192.169.0.1").to be_nil
+      response = subject.find("192.169.0.1")
+      expect(response).to be_nil
+    end
+  end
+
+  describe "when a range is added with metadata" do
+    subject do
+      IPRange::Range.new
+    end
+
+    before do
+      subject.remove("192.168.0.1/24")
+      subject.add("192.168.0.1/24", some: "data", more: "metadata")
+    end
+
+    it "should find it back" do
+      response = subject.find("192.168.0.20")
+      expect(response[:range]).to eq("192.168.0.1/24")
+      expect(response["some"]).to eq("data")
+      expect(response["more"]).to eq("metadata")
     end
   end
 end
